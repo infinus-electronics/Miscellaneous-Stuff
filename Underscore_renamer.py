2c02b7727a2ed5447747a121c7008a7bc989c9b4
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2020-12-14 23:32:10 +0800

    Added simple menus

--- Underscore_renamer.py
+++ Underscore_renamer.py
@@ -8,11 +8,35 @@ parent_path = path.rsplit("\\", 1)[0]
 
 files = os.listdir(parent_path)
 
+file_formats = []
+for _file in files:
+    if ('.'+_file.split('.')[1]) not in file_formats:
+        file_formats.append("."+_file.split('.')[1])
+print("File extensions found:")
+print(file_formats)
+instr = input("Select target file extensions with a comma seperated list(blank defaults to jpg and jpeg) \n")
+instr = instr.replace(".", '') #remove occurences of blankspaces and dots
+instr = instr.replace(" ", '')
+#print(instr)
+
+
+if len(instr) > 0: #user specified certain filenames
+    file_extensions = instr.split(',')
+else:
+    file_extensions = ['jpg','jpeg']
+
 
 #print(files)
-
+rename_count = 0
 for _file in files:
-    if _file.split('.')[1] == 'jpg' or _file.split('.')[1] == 'jpeg':
-        new_name = _file.replace(" ", "_")
-        os.rename(parent_path+"\\"+_file, parent_path+"\\"+new_name)
+    if _file.split('.')[1] in file_extensions:
+        rename_count = rename_count + 1
+
+confirmation = input("Rename {0} files? (Y/N) \n".format(rename_count))
+
+if confirmation == "y" or confirmation == "Y" or confirmation == "Yes" or confirmation == "yes":
+    for _file in files:
+       if _file.split('.')[1] in file_extensions:
+            new_name = _file.replace(" ", "_")
+            os.rename(parent_path+"\\"+_file, parent_path+"\\"+new_name)
 
