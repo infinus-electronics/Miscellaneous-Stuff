55e6d5b1699b257852e0fa54b931b7ba16dd96f4
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2020-12-14 16:38:31 +0800

    Replaces spaces with underscores

---
+++ Underscore_renamer.py
@@ -0,0 +1,19 @@
+from abc import get_cache_token
+import os
+
+path = os.path.realpath(__file__)
+
+parent_path = path.rsplit("\\", 1)[0]
+
+#print(parent_path)
+
+files = os.listdir(parent_path)
+
+
+#print(files)
+
+for _file in files:
+    if _file.split('.')[1] == 'jpg' or _file.split('.')[1] == 'jpeg':
+        new_name = _file.replace(" ", "_")
+        os.rename(parent_path+"\\"+_file, parent_path+"\\"+new_name)
+
