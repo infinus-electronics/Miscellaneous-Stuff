d1256324df8dfd772a55c8c3413e903fc74219b6
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2020-12-15 10:25:34 +0800

    Bug Fixes

--- Jupyter_CSS_Fixer.py
+++ Jupyter_CSS_Fixer.py
@@ -1,22 +1,25 @@
 import sys
 
-from tempfile import mksfile, mkstemp
-from shutil import copymode, mode, move
+from tempfile import mkstemp
+from shutil import copymode, move
 from os import fdopen, remove
 
-old_file_path = sys.argv[1]
-
+try:
+    old_file_path = sys.argv[1]
+except IndexError:
+    old_file_path = input('Specify Target Path\n')
 
 found_body = False
 file_handler, new_file_path = mkstemp()
-with fdopen(file_handler, 'w') as new_file:
-    with open(old_file) as old_file:
+with fdopen(file_handler, 'w', encoding="utf-8") as new_file:
+    with open(old_file_path, encoding="utf-8") as old_file:
         for line in old_file:
-
-            if line == "body{":
+            # debug note: use print(repr()) to show newline characters
+            if line == "body {\n":
                 found_body = True
+                print('found a body')
             
-            if "font-family: var(--jp-ui-font-family);" in line and flag == True:
+            if "font-family: var(--jp-ui-font-family);" in line and found_body == True:
                 new_file.write(line.replace('font-family: var(--jp-ui-font-family);', '/*font-family: var(--jp-ui-font-family);*/'))
                 found_body = False
                 continue
