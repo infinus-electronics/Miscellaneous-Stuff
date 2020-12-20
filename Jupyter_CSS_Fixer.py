2b31058905cb4d4b6845c1f2bde7944d44079873
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2020-12-21 00:13:39 +0800

    Now using iframes

--- Jupyter_CSS_Fixer.py
+++ Jupyter_CSS_Fixer.py
@@ -4,10 +4,15 @@ from tempfile import mkstemp
 from shutil import copymode, move
 from os import fdopen, remove
 
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+
 try:
     old_file_path = sys.argv[1]
 except IndexError:
-    old_file_path = input('Specify Target Path\n')
+    old_file_path = askopenfilename()
 
 found_body = False
 file_handler, new_file_path = mkstemp()
@@ -15,12 +20,13 @@ with fdopen(file_handler, 'w', encoding="utf-8") as new_file:
     with open(old_file_path, encoding="utf-8") as old_file:
         for line in old_file:
             # debug note: use print(repr()) to show newline characters
-            if line == "body {\n":
+            if line == ".CodeMirror.cm-s-jupyter {\n":
                 found_body = True
-                print('found a body')
+                #print('found a body')
             
-            if "font-family: var(--jp-ui-font-family);" in line and found_body == True:
-                new_file.write(line.replace('font-family: var(--jp-ui-font-family);', '/*font-family: var(--jp-ui-font-family);*/'))
+            if "color: var(--jp-content-font-color1);" in line and found_body == True:
+                new_file.write(line)
+                new_file.write('  overflow: auto;\n')
                 found_body = False
                 continue
 
